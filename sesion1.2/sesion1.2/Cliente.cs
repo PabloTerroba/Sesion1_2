16f30f66881c3ce91f7048568de82bf01ee6139b
PabloTerroba <uo289073@uniovi.es>
2025-02-11 12:05:25 +0100

    Add files via upload
    Solución del proyecto de la Sesión 1.2

---
+++ sesion1.2/sesion1.2/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class SNFClient
+{
+    static void Main(string[] args)
+    {
+        UdpClient udpClient = new UdpClient();
+        IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 11000);
+        byte sequenceNumber = 0;
+        byte[] dataToSend = new byte[] { 1, 2, 3, 4, 5 };
+
+        foreach (byte data in dataToSend)
+        {
+            bool ackReceived = false;
+            while (!ackReceived)
+            {
+                SNFMessage message = new SNFMessage { SequenceNumber = sequenceNumber, Data = data };
+                udpClient.Send(message.Encode(), message.Encode().Length, serverEP);
+                Console.WriteLine($"Sent: {data}");
+
+                udpClient.Client.ReceiveTimeout = 1000; // 1 second timeout
+                try
+                {
+                    byte[] receivedData = udpClient.Receive(ref serverEP);
+                    SNFMessage ack = SNFMessage.Decode(receivedData);
+                    if (ack.IsAck && ack.SequenceNumber == sequenceNumber)
+                    {
+                        ackReceived = true;
+                        sequenceNumber++;
+                    }
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Timeout, resending...");
+                }
+            }
+        }
+
+        Console.WriteLine("All data sent successfully.");
+    }
+}
+
+
---
+++ sesion1.2/sesion1.2/Servidor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class SNFServer
+{
+    static void Main(string[] args)
+    {
+        UdpClient udpServer = new UdpClient(11000);
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+        byte expectedSequence = 0;
+
+        Console.WriteLine("Server started. Waiting for data...");
+
+        while (true)
+        {
+            byte[] receivedData = udpServer.Receive(ref remoteEP);
+            SNFMessage message = SNFMessage.Decode(receivedData);
+
+            if (message.SequenceNumber == expectedSequence)
+            {
+                Console.WriteLine($"Received: {message.Data}");
+                SNFMessage ack = new SNFMessage { SequenceNumber = expectedSequence, IsAck = true };
+                udpServer.Send(ack.Encode(), ack.Encode().Length, remoteEP);
+                expectedSequence++;
+            }
+            else
+            {
+                Console.WriteLine($"Discarded out-of-sequence packet: {message.SequenceNumber}");
+            }
+        }
+    }
+}
+
+
