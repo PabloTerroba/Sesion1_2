6c0a4a7bcbbc1b0ecbfc0c83aa501b6dd053ce87
brunololoneira <brunololoneira@gmail.com>
2025-02-18 01:16:53 +0100

    Add files via upload
    Ampliacion 1

---
+++ SNFProtocol/SNFProtocol/ICodecDATA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SNFProtocol
+{
+    internal interface ICodecDATA : ICodec
+    {
+        byte Data { get; set; }
+        byte SequenceNumber { get; set; }
+
+    }
+}
