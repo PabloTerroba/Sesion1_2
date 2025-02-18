6bb29cb35d384b126bde8b6be685c4f4eef3e48d
PabloTerroba <uo289073@uniovi.es>
2025-02-18 10:56:53 +0100

    Solución del proyecto para la ampliación 2.
    Modificación de los test unitarios.

---
+++ Sesion1_2-ampliacion2/SNFProtocol/SNFProtocol/ICodec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace SNFProtocol
+{
+    public interface ICodec
+    {
+        byte[] Encode();
+        void Decode(byte[] data);
+    }
+}
